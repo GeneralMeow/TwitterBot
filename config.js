4bc4fb0f196f388714670f0a490e04b7728a4c00
generalmeow <rcbrown@gmail.com>
2017-03-23 10:41:15 -0700

    add keys and tokens to config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+/*
+TWITTER APP CONFIGURATION
+*consumer_key
+*consumer_secret
+*access_token
+*access_token_secret
+*/
+
+module.exports = {
+  consumer_key: 'n9vUPDgXlOCUnRhNeX632DvMN',
+  consumer_secret: 'NwK7ije8mKE13RxTJoVtdXgGHSyO8GcW3Lj9eVOd9o9IgUK81I',
+  access_token: '841705401095606272-JLny9Gn1KXrfRESCv101EshjC1UR6gs',
+  access_token_secret: '5ifsaBYFDtxaXshX8ofIAel0mLgxs9XcCeOP7a0NOv9zV'
+}
